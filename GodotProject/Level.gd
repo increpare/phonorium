ad93c88b0ee69c30468c95d1c377b000504e4f9b
Stephen Lavelle <analytic@gmail.com>
2022-03-20 02:52:19 +0100

    start work on save

--- GodotProject/Level.gd
+++ GodotProject/Level.gd
@@ -15,13 +15,39 @@ func findByClass(node: Node, classPath : String, result : Array) -> void:
 	for child in node.get_children():
 		findByClass(child, classPath, result)
 
-
+func save_level():	
+	var solvedarray = []
+	for n in booths.size():
+		var booth = booths[n]
+		solvedarray.append(booth.solved)
+	var save_file = File.new()
+	save_file.open("user://blag.save", File.WRITE)
+	save_file.store_line(JSON.print(solvedarray))
+	save_file.close()
+		
+func load_level():
+	var save_file = File.new()
+	if not save_file.file_exists("user://blag.save"):
+		return
+		
+	save_file.open("user://blag.save", File.READ)
+	print(save_file.get_as_text())
+	print( "{solved:"+save_file.get_as_text()+"}")
+	var solved_data = JSON.parse("{solved:"+save_file.get_as_text()+"}").solved
+	save_file.close()
+	
+	for n in booths.size():
+		if solved_data[n]:
+			var booth = booths[n]	
+			booth.setsolved()
+		
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	booths=[]
 	findByClass(self,"res://booth.gd",booths);
 	flames=[]
 	findByClass(self,"res://Flame.gd",flames);
+	#load_level()
 
 func solvedgroup(b:Node):
 	var group:String = b.group
@@ -43,6 +69,7 @@ func onsolve(b:Node):
 			print(flame.GroupName)
 			if flame.GroupName==group:
 				flame.visible=false
+	#save_level()
 	return groupsolved
 
 func _process(d):
