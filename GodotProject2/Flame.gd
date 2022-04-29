ed3184fc24afa2b46ac954a98b75ab0ab0be3101
Stephen Lavelle <analytic@gmail.com>
2022-04-29 20:09:49 +0200

    .

--- GodotProject2/Flame.gd
+++ GodotProject2/Flame.gd
@@ -1,8 +1,8 @@
-extends AnimatedSprite3D
+extends Node
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	get_child(0).text=get_parent().name
+#func _ready():
+	#get_child(0).text=get_parent().name
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
